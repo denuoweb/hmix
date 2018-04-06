31494ca040f75357f9d77619bab9cff7c1926839
spaceman <dev@blockchainspaceman.com>
2018-04-05 23:00:54 -0500

    switched kfichter qtum portal to spacemanholdings qtum portal (built from same image)

--- src/client/app/shared/components/sidebar/help-tab/help-tab.component.ts
+++ src/client/app/shared/components/sidebar/help-tab/help-tab.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HelpTabComponent {
   private helpCommands = {
-    'docker': '$ docker run -it --rm --name myapp -v `pwd`:/dapp -p 9899:9899 -p 9888:9888 -p 3889:3889 kfichter/qtum-portal-dev',
+    'docker': '$ docker run -it --rm --name myapp -v `pwd`:/dapp -p 9899:9899 -p 9888:9888 -p 3889:3889 spacemanholdings/qtum-portal-dev',
     'generateInitial': '$ docker exec -it myapp sh \n' +
                        '$ qcli generate 600',
     'generateOne': '$ qcli generate 1'
