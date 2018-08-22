46062f43be729060cadb7df40e02280ac80f788c
Devanshu Bharel <Brownie79@users.noreply.github.com>
2018-08-21 22:02:21 -0500

    updated helptext with spc docker container which builds from latest qtum release

--- src/client/app/shared/constants/help-text.ts
+++ src/client/app/shared/constants/help-text.ts
@@ -1,6 +1,6 @@
 //help text, update docker instructions if different regtest used
 export const HELP_COMMANDS = {
-  docker: '$ docker run -it --rm --name myapp -v `pwd`:/dapp -p 9899:9899 -p 9888:9888 -p 3889:3889 kfichter/qtum-portal-dev',
+  docker: '$ docker run -it --rm --name myapp -v `pwd`:/dapp -p 9899:9899 -p 9888:9888 -p 3889:3889 spacemanholdings/qtum-portal-dev',
   generateInitial: '$ docker exec -it myapp sh \n' +
                    '$ qcli generate 600',
   generateOne: '$ qcli generate 1'
