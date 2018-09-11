a6b9ac981a4dad253b2374a1f46964eed1c33b7a
Devanshu Bharel <Brownie79@users.noreply.github.com>
2018-09-10 22:31:10 -0500

    fixed typo

--- src/client/app/shared/constants/help-text.ts
+++ src/client/app/shared/constants/help-text.ts
@@ -1,6 +1,6 @@
 //help text, update docker instructions if different regtest used
 export const HELP_COMMANDS = {
   docker: '$ docker run -d --rm --name myapp -v -p 9899:9899 -p 9888:9888 -p 3889:3889 spacemanholdings/qtum-portal-dev',
-  generateInitial: '$ docker exec myapp qcli generate 600'
-  generateOne: '$ qcli generate 1'
+  generateInitial: '$ docker exec myapp qcli generate 600',
+  generateOne: '$ docker exec myapp qcli generate 1'
 };
